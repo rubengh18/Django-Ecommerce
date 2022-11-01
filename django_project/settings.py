7828296f4e05e4b4ae1be54383a88987c6412fee
rubengh18 <arubengh18@gmail.com>
2022-10-31 22:18:30 -0500

    vercel config

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,7 +31,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # This is necessary because otherwise Gunicorn will reject the connections
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
 }
 
 
